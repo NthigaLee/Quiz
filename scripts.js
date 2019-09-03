0e4bd569e7c67de958b351b9eaf1dbd5e6f00a1e
NthigaLee <nthigalee@gmail.com>
2019-09-03 21:47:05 +0300

    Final updates

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
         
         $(".clickable"+diff).click(function(){
             $(".questions").hide();
+            
             $(".questions#" + diff).fadeToggle(1000);
             
         }); 
@@ -20,12 +21,13 @@ $(document).ready(function(){
             var b=parseInt($("input:radio[name=answer2]:checked").val());
             var c=parseInt($("input:radio[name=answer3]:checked").val());
             var result= a+b+c;
-        $(".questions").hide();  
+        $(".well").hide();  
         $(".score").fadeToggle(1000);
         $("#output").text(result);
         
             
     });  
+
 });
 });   
    
